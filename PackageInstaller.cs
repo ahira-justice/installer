870a34909d1a0938d4eb847f0c2ea94683fe1c9c
Ahira <justiceahira@gmail.com>
2020-07-27 22:23:55 +0100

    Create PackageInstaller class

---
+++ PackageInstaller.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace Installer
+{
+
+    public class PackageInstaller {
+
+        private BackgroundWorker _extractWorker = new BackgroundWorker();
+        private string _archivePath;
+        private string _installPath;
+
+        public long ArchiveSize { get; private set; }
+        public long TotalInstalledSize { get; private set; }
+
+        public PackageInstaller(string archivePath, string installPath) {
+            _extractWorker.DoWork += ExtractFile;
+            _extractWorker.WorkerReportsProgress = true;
+            _extractWorker.WorkerSupportsCancellation = true;
+
+            _archivePath = archivePath;
+            _installPath = installPath;
+        }
+
+        private void ExtractFile(object sender, DoWorkEventArgs e) { }
+
+    }
+
+}
